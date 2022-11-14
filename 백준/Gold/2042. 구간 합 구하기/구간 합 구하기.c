6a61e863cf499fb08dc5f6310dcd4cfb2262bac7
jukanmi <100185252+jukanmi@users.noreply.github.com>
2022-11-14 23:19:06 +0900

    [Gold I] Title: 구간 합 구하기, Time: 352 ms, Memory: 40176 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.c
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.c
@@ -1,43 +1,39 @@
 #include <stdio.h>
 #define ll long long
-ll init(int start, int end, int node, ll ST[], ll arr[]);
-ll sum(int start, int end, int node, int left, ll right, ll ST[]);
-void update(int start, int end, int node, int index, ll dif, ll ST[], ll a[]);
+ll init(int start, int end, int node);
+ll sum(int start, int end, int node, int left, ll right);
+void update(int start, int end, int node, int index, ll dif);
+ll ST[4000001], arr[1000001];
 int main() {
     int N, M, K, a, b, len = 0;
-    ll seg_tree[4000001] = { 0 }, k[10001], arr[1000001], c;
+    ll k[10001], c;
     scanf("%d%d%d", &N, &M, &K);
     for (int i = 0; i < N; i++)
         scanf("%lld", &arr[i]);
-    init(0, N - 1, 1, seg_tree, arr);
+    init(0, N - 1, 1);
     for (int i = 0; i < M + K; i++) {
         scanf("%d%d%lld", &a, &b, &c);
         if (a == 1)
-            update(0, N - 1, 1, b-1, c - arr[b-1], seg_tree,arr);
+            update(0, N - 1, 1, b-1, c - arr[b-1]);
         else
-            k[len++] = sum(0, N - 1, 1, b-1, c-1, seg_tree);
+            k[len++] = sum(0, N - 1, 1, b-1, c-1);
     }
     for (int i = 0; i < K; i++)
         printf("%lld\n", k[i]);
-    /*
-    for (int i = 1; seg_tree[i] != 0; i++)
-        printf("%lld ", seg_tree[i]);
-    printf("\n");
-    */
     return 0;
 }
-ll init(int start, int end, int node, ll ST[], ll arr[]) {
+ll init(int start, int end, int node) {
     if (start == end) return ST[node] = arr[start];
     int mid = (start + end) / 2;
-    return ST[node] = init(start, mid, node * 2, ST, arr) + init(mid + 1, end, node * 2 + 1, ST, arr);
+    return ST[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
 }
-ll sum(int start, int end, int node, int left, ll right, ll ST[]) {
+ll sum(int start, int end, int node, int left, ll right) {
     if (left > end || right < start) return 0;
     if (left <= start && end <= right) return ST[node];
     int mid = (start + end) / 2;
-    return sum(start, mid, node * 2, left, right, ST) + sum(mid + 1, end, node * 2 + 1, left, right, ST);
+    return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
 }
-void update(int start, int end, int node, int index, ll dif, ll ST[], ll arr[]) {
+void update(int start, int end, int node, int index, ll dif) {
     if (index<start || index>end) return;
     ST[node] += dif;
     if (start == end) {
@@ -45,6 +41,6 @@ void update(int start, int end, int node, int index, ll dif, ll ST[], ll arr[]) 
         return;
     }
     int mid = (start + end) / 2;
-    update(start, mid, node * 2, index, dif, ST, arr);
-    update(mid + 1, end, node * 2 + 1, index, dif, ST, arr);
+    update(start, mid, node * 2, index, dif);
+    update(mid + 1, end, node * 2 + 1, index, dif);
 }
