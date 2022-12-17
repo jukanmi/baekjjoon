5ae2ba269c5524f0a786148ebb44ccdb677f8a66
jukanmi <100185252+jukanmi@users.noreply.github.com>
2022-12-17 22:18:29 +0900

    [Bronze IV] Title: 빅데이터? 정보보호!, Time: 32 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/26264. 빅데이터？ 정보보호！/빅데이터？ 정보보호！.c
+++ 백준/Bronze/26264. 빅데이터？ 정보보호！/빅데이터？ 정보보호！.c
@@ -5,15 +5,15 @@ int main(){
         scanf("%c",&a);
         if (a==10) break;
     }
-    int b[2]={0};
+    int b=0;
     while (1){
         scanf("%c",&a);
         if (a==10) break;
-        if (a=='b') b[0]++;
-        if (a=='s') b[1]++;
+        if (a=='b') b++;
+        if (a=='s') b--;
     }
-    if (b[0]<b[1]) printf("security!\n");
-    else if (b[0]>b[1]) printf("bigdata?\n");
+    if (b<0) printf("security!\n");
+    else if (0<b) printf("bigdata?\n");
     else printf("bigdata? security!\n");
     return 0;
 }
