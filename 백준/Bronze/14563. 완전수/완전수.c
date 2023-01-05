e7b50f028bc28497eeca4ea4df4de216f91fabe4
jukanmi <100185252+jukanmi@users.noreply.github.com>
2023-01-05 16:10:17 +0900

    [Bronze II] Title: 완전수, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/14563. 완전수/완전수.c
+++ 백준/Bronze/14563. 완전수/완전수.c
@@ -1,15 +1,1 @@
-#include <stdio.h>
-int main() {
-  int T,N;
-  scanf("%d",&T);
-  while (T--){
-    int sum=0;
-    scanf("%d",&N);
-    for (int i=1;i<N;i++)
-      if (N%i==0)
-        sum+=i;
-    if (sum<N)puts("Deficient");
-    else if (sum>N)puts("Abundant");
-    else puts("Perfect");
-  }
-}
+main(T,N) {scanf("%d",&T);while (T--){int sum=0;scanf("%d",&N);for (int i=1;i<N;i++)if (N%i==0)sum+=i;if (sum<N)puts("Deficient");else if (sum>N)puts("Abundant");else puts("Perfect");}}
