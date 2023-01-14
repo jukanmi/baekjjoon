5f89fa4eb6df5a21b8aae06a952faf2dfe0d278a
jukanmi <100185252+jukanmi@users.noreply.github.com>
2023-01-14 22:12:21 +0900

    [Silver III] Title: 이전 순열, Time: 0 ms, Memory: 1164 KB -BaekjoonHub

--- 백준/Silver/10973. 이전 순열/이전 순열.c
+++ 백준/Silver/10973. 이전 순열/이전 순열.c
@@ -5,32 +5,23 @@ int N, arr[10000], i;
 bool idx[10001];
 int main() {
     scanf("%d", &N);
-    for (i = 0; i < N; i++) {
+    for (i = 0; i < N; i++)
         scanf("%d", &arr[i]);
-    }
     for (i = N - 1; i > 0; i--) {
         idx[arr[i]] = true;
-        if (arr[i] < arr[i - 1]){
-            int a = i-1;
-            idx[arr[a]] = true;
-            for (int j = arr[i-1] - 1; j >=0 ; j--) {
-                if (idx[j]) {
-                    idx[j] = false;
-                    arr[a++] = j;
-                    break;
-                }
-            }
-            for (int j = N; j >0; j--) {
-                if (idx[j]) {
-                    arr[a++] = j;
-                }
-            }
+        if (arr[i] < arr[i - 1])
             break;
-        }
     }
-    if (!i)
+    if (!i--)
         puts("-1");
-    else
-        for (i = 0; i < N; i++)
-            printf("%d ", arr[i]);
+    else {
+        for (int j = 0; j < i; j++)printf("%d ", arr[j]);
+        idx[arr[i]] = true;
+        while (!idx[--arr[i]]);
+        printf("%d ", arr[i]);
+        idx[arr[i]] = false;
+        for (i = N; i > 0; i--)
+            if (idx[i])
+                printf("%d ", i);
+    }
 }
