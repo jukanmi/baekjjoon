96a9337327aaea528affb2aab1806aabcef05af8
jukanmi <100185252+jukanmi@users.noreply.github.com>
2023-01-24 20:03:39 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5028 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char L[1001], S[1001];
+int plus[1001][1001];
+int maxx(int a, int b) { return a > b ? a : b; }
+int main() {
+    scanf("%s%s", L, S);
+    int lenL = strlen(L), lenS = strlen(S);
+    for (int i = 1; i <= lenL; i++) {
+        for (int j = 1; j <= lenS; j++) {
+            if (L[i-1] == S[j-1])
+                plus[i][j] = plus[i - 1][j - 1] + 1;
+            else
+                plus[i][j] = maxx(plus[i - 1][j],plus[i][j-1]);
+        }
+    }
+    int max = 0;
+    for (int i = 1; i <= lenS; i++)
+        max = maxx(plus[lenL][i],max);
+    printf("%d", max);
+}
