654ae1c4e0fb2354086280eba7b98fd92b3e71e1
jukanmi <100185252+jukanmi@users.noreply.github.com>
2023-02-09 00:19:50 +0900

    [Silver I] Title: 계단, Time: 24 ms, Memory: 1776 KB -BaekjoonHub

--- 백준/Silver/21600. 계단/계단.c
+++ 백준/Silver/21600. 계단/계단.c
@@ -5,12 +5,11 @@ int main() {
 	scanf("%d",&N);
 	for (int i=0;i<N;i++)
 		scanf("%d",&arr[i]);
+	int max=0;
 	for (int i=1;i<N;i++){
 		if (dp[i-1]+1<=arr[i]) dp[i]=dp[i-1]+1;
 		else dp[i]=arr[i];
-	}
-	int max=0;
-	for (int i=0;i<N;i++)
 		max=max<dp[i]?dp[i]:max;
+	}
 	printf("%d\n",max);
 }
