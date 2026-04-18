0b90eb522df8ca7100249ca0d40253f95f2f626b
정광민 <100185252+jukanmi@users.noreply.github.com>
2026-04-18 20:28:35 +0900

    [Gold IV] Title: Chance!, Time: 24 ms, Memory: 6128 KB -BaekjoonHub

--- 백준/Gold/31804. Chance！/Chance！.cc
+++ 백준/Gold/31804. Chance！/Chance！.cc
@@ -1,44 +1,65 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include <queue>
-#include <unordered_set>
+#include <algorithm>
+#define MAX 1000001
+#define INF 2e9
+#define FASTIO cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
 
 using namespace std;
+int A, B;
+bool Visited[MAX][2];
+int Answer = INF;
 
-struct State {
-    int value;
-    int steps;
-    bool used_chance;
-};
+void input() {
+    cin >> A >> B;
+}
 
-int solve(int a, int b) {
-    queue<State> q;
-    q.push({a, 0, false});
-    unordered_set<long long> visited;
+void bfs() {
+    queue<pair<pair<int, int>, bool> > Q;
+    Q.push(make_pair(make_pair(A, 0), false));
+    Visited[A][0] = true;
 
-    while (!q.empty()) {
-        State cur = q.front(); q.pop();
+    while (!Q.empty()) {
+        int NowS = Q.front().first.first;
+        int NowM = Q.front().first.second;
+        bool isChanceUsed = Q.front().second;
+        Q.pop();
 
-        if (cur.value == b) return cur.steps;
-        long long key = (long long)cur.value * 2 + cur.used_chance;
-        if (visited.count(key)) continue;
-        visited.insert(key);
+        if (NowS == B) {
+            Answer = min(Answer, NowM);
+            continue;
+        }
 
-        if (cur.value + 1 <= 1000000)
-            q.push({cur.value + 1, cur.steps + 1, cur.used_chance});
+        if ((NowS + 1 <= B) && !Visited[NowS + 1][isChanceUsed]) {
+            Q.push(make_pair(make_pair(NowS + 1, NowM + 1), isChanceUsed));
+            Visited[NowS + 1][isChanceUsed] = true;
+        }
+        if ((NowS * 2 <= B) && !Visited[NowS * 2][isChanceUsed]) {
+            Q.push(make_pair(make_pair(NowS * 2, NowM + 1), isChanceUsed));
+            Visited[NowS * 2][isChanceUsed] = true;
+        }
+        if (!isChanceUsed && (NowS * 10 <= B) && !Visited[NowS * 10][isChanceUsed]) {
+            Q.push(make_pair(make_pair(NowS * 10, NowM + 1), true));
+            Visited[NowS * 10][isChanceUsed] = true;
+        }
+    };
+}
 
-        if (cur.value * 2 <= 1000000)
-            q.push({cur.value * 2, cur.steps + 1, cur.used_chance});
+void settings() {
+    bfs();
+}
 
-        if (!cur.used_chance && cur.value * 10 <= 1000000)
-            q.push({cur.value * 10, cur.steps + 1, true});
-    }
-
-    return -1;
+void find_Answer() {
+    cout << Answer << "\n";
 }
 
 int main() {
-    int a, b;
-    cin >> a >> b;
-    cout << solve(a, b) << endl;
+    FASTIO
+    input();
+    settings();
+    find_Answer();
+
     return 0;
 }
