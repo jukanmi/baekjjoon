8e482122d490f45f518dc291eaf0848369655449
정광민 <100185252+jukanmi@users.noreply.github.com>
2026-04-18 20:46:29 +0900

    [Gold V] Title: 가희와 프로세스 1, Time: 348 ms, Memory: 4336 KB -BaekjoonHub

--- 백준/Gold/21773. 가희와 프로세스 1/가희와 프로세스 1.cc
+++ 백준/Gold/21773. 가희와 프로세스 1/가희와 프로세스 1.cc
@@ -1,40 +1,34 @@
-
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
 using namespace std;
-using vi = vector<int>;
-#define endl "\n"
-#define FIO                                                                    \
-  ios_base::sync_with_stdio(0);                                                \
-  cin.tie(0);                                                                  \
-  cout.tie(0);
-#define all(x) x.begin(), x.end()
-const int MAXN = 1e5 + 4;
-int N, T;
-priority_queue<array<int, 3>> process;
-void init() {
-  cin >> T >> N;
-  for (int i = 0; i < N; i++) {
-    int a, b, c;
-    cin >> a >> b >> c;
-    process.push({c, -a, b});
-  }
+int T;
+priority_queue <std::pair <int, std::pair <int, int>>> Q;
+
+void Input() {
+	int N;
+	cin >> T >> N;
+	for (int i = 0; i < N; i++) {
+		int A, B, C;
+		cin >> A >> B >> C;
+		Q.push({ C, {-A, B} });
+	}
 }
-void solve() {
-  while (T--) {
-    auto [c, a, b] = process.top();
-    process.pop();
-    cout << -a << endl;
-    if (b > 1) {
-      process.push({c - 1, a, b - 1});
-    }
-  }
+
+void Solve() {
+	while (T && !Q.empty()) {
+		int C = Q.top().first;
+		int A = Q.top().second.first;
+		int B = Q.top().second.second;
+		Q.pop();
+		cout << -A << "\n";
+		C--;
+		B--;
+		if (B != 0) Q.push({ C,{A,B} });
+		T--;
+	}
 }
+
 int main() {
-  FIO;
-#ifndef BOJ
-  freopen("input.txt", "r", stdin);
-#endif
-  init();
-  solve();
-  return 0;
+	Input();
+	Solve();
 }
