92f11e09db3a739c2293c3679228c961c803d3aa
정광민 <100185252+jukanmi@users.noreply.github.com>
2026-04-18 20:47:58 +0900

    [Platinum III] Title: 열혈강호 3, Time: 384 ms, Memory: 4192 KB -BaekjoonHub

--- 백준/Platinum/11377. 열혈강호 3/열혈강호 3.cc
+++ 백준/Platinum/11377. 열혈강호 3/열혈강호 3.cc
@@ -9,7 +9,6 @@ using vi = vector<int>;
 const int MAXN = 1e4 + 4;
 bool match[MAXN];  // 매칭 상태
 int matched[MAXN]; // 매칭된 상대
-bool worked[MAXN]; // 해당 상대가 이미 매칭되었는지
 int N, M, K;
 
 bool dfs(int node, vector<vi> &edges) {
@@ -41,13 +40,13 @@ int main() {
   }
   int ans = 0;
   for (int i = 1; i <= N; i++) {
-    fill(match, match + MAXN, 0);
+    fill_n(match, MAXN, 0);
     if (dfs(i, edges)) {
       ans++;
     }
   }
   for (int i = 1; i <= N && K > 0; i++) {
-    fill(match, match + MAXN, 0);
+    fill_n(match, MAXN, 0);
     if (dfs(i, edges)) {
       ans++, K--;
     }
